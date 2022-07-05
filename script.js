fb6472d3348bd8a92d57abba5229d35f6768f22b
Alec <alecsosa223@gmail.com>
2022-07-05 13:31:14 -0500

    Created index, script page and style sheet
    Created basic sheets for Etch-a-Sketch project

---
+++ script.js
@@ -0,0 +1,12 @@
+/**
+ * Etch-a-Sketch
+ * 
+ * Author: Alec Sosa
+ * Date: 7/5/2022
+ * 
+ * Filename: script.js
+ */
+
+const container = document.querySelector('#grid-container');
+const div_Squares = document.createElement('div');
+div_Squares.classList.add('square');
