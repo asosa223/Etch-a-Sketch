8df1e98abe638a1e76d57222fc5215d954173be1
Alec <alecsosa223@gmail.com>
2022-07-06 12:32:01 -0500

    Created 16x16 grid
    using flex grid and flex box, I was able to create a 16x16 grid of
    boxes

--- script.js
+++ script.js
@@ -7,6 +7,17 @@
  * Filename: script.js
  */
 
-const container = document.querySelector('#grid-container');
-const div_Squares = document.createElement('div');
-div_Squares.classList.add('square');
+const container = document.querySelector('.grid-container');
+
+function makeRows(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+
+    for (c = 0; c < (rows * cols); c++){
+        let cell = document.createElement('div');
+        cell.innerText = c + 1;
+        container.appendChild(cell).className = 'grid-item';
+    };
+};
+
+makeRows(16, 16);
